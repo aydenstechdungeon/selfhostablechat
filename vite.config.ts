f5220e9c784e24dc9d56ae0e6ace8ced21bc84b2
Your Name <sblolroblox@gmail.com>
2026-02-02 23:05:09 -0500

    build(vite): disable css code splitting
    Disable CSS code splitting to avoid preload warnings.
    CSS will be inlined into JS chunks instead of separate files.

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		// Disable CSS code splitting to avoid preload warnings
+		// CSS will be inlined into JS chunks instead of separate files
+		cssCodeSplit: false
+	}
 });
